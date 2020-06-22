6789837eea98ac17c8d83c1c06b61d6e712ff200
kazedayo <winsonlam@me.com>
2020-06-22 11:35:06 +0800

    refactor home page

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -3,16 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:hkgalden_flutter/models/thread.dart';
 import 'package:hkgalden_flutter/redux/app/app_state.dart';
 import 'package:hkgalden_flutter/redux/store.dart';
 import 'package:hkgalden_flutter/redux/thread_list/thread_list_action.dart';
-import 'package:hkgalden_flutter/ui/common/page_end_loading_indicator.dart';
 import 'package:hkgalden_flutter/ui/home/compose_page.dart';
 import 'package:hkgalden_flutter/ui/home/drawer/home_drawer.dart';
-import 'package:hkgalden_flutter/ui/home/thread_cell.dart';
 import 'package:hkgalden_flutter/ui/page_transitions.dart';
-import 'package:hkgalden_flutter/ui/thread/thread_page.dart';
 import 'package:hkgalden_flutter/viewmodels/home_page_view_model.dart';
 
 class HomePage extends StatefulWidget {
@@ -90,25 +86,7 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
           onRefresh: () => viewModel.onRefresh(viewModel.selectedChannelId),
           child: ListView(
             controller: _scrollController,
-            children: <Widget>[
-              for (Thread thread in viewModel.threads)
-                ThreadCell(
-                  title: thread.title,
-                  authorName: thread.replies[0].authorNickname,
-                  totalReplies: thread.totalReplies,
-                  lastReply: thread.replies.length == 2 ? 
-                              thread.replies[1].date : 
-                              thread.replies[0].date,
-                  onTap: () {
-                    viewModel.onThreadCellTap(thread.threadId);
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => ThreadPage(
-                      title: thread.title,
-                      threadId: thread.threadId,
-                    )));
-                  },
-                ),
-              PageEndLoadingInidicator(),
-            ],
+            children: viewModel.onLoadThreadList(context),
           ),
         ),
         drawer: HomeDrawer(),
--- lib/viewmodels/home_page_view_model.dart
+++ lib/viewmodels/home_page_view_model.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:hkgalden_flutter/models/thread.dart';
 import 'package:hkgalden_flutter/redux/app/app_state.dart';
+import 'package:hkgalden_flutter/ui/common/page_end_loading_indicator.dart';
+import 'package:hkgalden_flutter/ui/home/thread_cell.dart';
+import 'package:hkgalden_flutter/ui/thread/thread_page.dart';
 import 'package:redux/redux.dart';
 import 'package:hkgalden_flutter/redux/thread_list/thread_list_action.dart';
 import 'package:hkgalden_flutter/redux/thread/thread_action.dart';
@@ -7,20 +11,20 @@ import 'package:hkgalden_flutter/redux/thread/thread_action.dart';
 class HomePageViewModel {
   final List<Thread> threads;
   final String title;
-  final Function(String) onRefresh;
-  final Function(int) onThreadCellTap;
   final String selectedChannelId;
   final bool isThreadLoading;
   final bool isRefresh;
+  final Function(String) onRefresh;
+  final Function(BuildContext) onLoadThreadList;
 
   HomePageViewModel({
     this.threads,
     this.title,
-    this.onRefresh,
-    this.onThreadCellTap,
     this.selectedChannelId,
     this.isThreadLoading,
     this.isRefresh,
+    this.onRefresh,
+    this.onLoadThreadList,
   });
 
   factory HomePageViewModel.create(Store<AppState> store) {
@@ -29,11 +33,31 @@ class HomePageViewModel {
       title: store.state.channelState.channels.where(
         (channel) => channel.channelId == store.state.channelState.selectedChannelId)
       .first.channelName,
-      onRefresh: (channelId) => store.dispatch(RequestThreadListAction(channelId: channelId, page: 1,isRefresh: true)),
-      onThreadCellTap: (threadId) => store.dispatch(RequestThreadAction(threadId: threadId, page: 1, isInitialLoad: true)),
       selectedChannelId: store.state.channelState.selectedChannelId,
       isThreadLoading: store.state.threadListState.threadListIsLoading,
       isRefresh: store.state.threadListState.isRefresh,
+      onRefresh: (channelId) => store.dispatch(RequestThreadListAction(channelId: channelId, page: 1,isRefresh: true)),
+      onLoadThreadList: (BuildContext context) {
+        List<Widget> threadCells = [];
+        for (Thread thread in store.state.threadListState.threads)
+          threadCells.add(ThreadCell(
+            title: thread.title,
+            authorName: thread.replies[0].authorNickname,
+            totalReplies: thread.totalReplies,
+            lastReply: thread.replies.length == 2 ? 
+                        thread.replies[1].date : 
+                        thread.replies[0].date,
+            onTap: () {
+              store.dispatch(RequestThreadAction(threadId: thread.threadId, page: 1, isInitialLoad: true));
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => ThreadPage(
+                title: thread.title,
+                threadId: thread.threadId,
+              )));
+            },
+          ));
+        threadCells.add(PageEndLoadingInidicator());
+        return threadCells;
+      }
     );
   }
 }
