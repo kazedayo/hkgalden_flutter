04846c8b37c0cdb54f47814e22b5ff218226e0bb
kazedayo <winsonlam@me.com>
2020-06-23 14:35:30 +0800

    color font fix attempt

--- lib/ui/thread/comment_cell.dart
+++ lib/ui/thread/comment_cell.dart
@@ -1,6 +1,7 @@
 import 'package:date_time_format/date_time_format.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_html/html_parser.dart';
 import 'package:flutter_html/style.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hkgalden_flutter/models/reply.dart';
@@ -67,7 +68,7 @@ class CommentCell extends StatelessWidget {
               shrinkWrap: true,
               data: HKGaldenHtmlParser().commentWithQuotes(reply),
               customRender: {
-                'img': (context, child, attributes, node) {
+                'img': (context, _, attributes, __) {
                   return GestureDetector(
                     onTap: () => _showImageView(context.buildContext, attributes['src'], '${reply.floor}_${attributes['src']}'),
                     child: Hero(
@@ -89,12 +90,26 @@ class CommentCell extends StatelessWidget {
                     ),
                   );
                 },
-                'icon': (context, child, attributes, node) {
+                'icon': (_, __, attributes, ____) {
                   return CachedNetworkImage(
                     imageUrl: attributes['src'],
                     fadeInDuration: Duration(milliseconds: 300),
                     fadeOutDuration: Duration(milliseconds: 300),
                   );
+                },
+                'span': (context, child, attributes, element) {
+                  if (attributes.containsKey('color')) {
+                    Style newStyle = context.style.copyWith(
+                      color: Color(int.parse('FF${attributes['hex']}'))
+                    );
+                    return ContainerSpan(
+                      newContext: RenderContext(buildContext: context.buildContext, style: newStyle, parser: context.parser),
+                      style: newStyle,
+                      children: (child as ContainerSpan).children,
+                      child: (child as ContainerSpan).child,
+                    );
+                  }
+                  return child;
                 }
               },
               style: {
