115895ad7b3e0984cd3c5a0483cb1dbc7c781d61
kazedayo <winsonlam@me.com>
2020-06-30 16:23:52 +0800

    title whitespace

--- lib/ui/home/thread_cell.dart
+++ lib/ui/home/thread_cell.dart
@@ -20,53 +20,56 @@ class ThreadCell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Column(
-    children: <Widget>[
-      InkWell(
-        child: Container(
-          padding: EdgeInsets.all(8),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(title, style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700)),
-              SizedBox(height: 10),
-              Container(
-                constraints: BoxConstraints.expand(height: 20),
-                child: Row(
-                  //crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(Icons.face, size: 15),
-                    SizedBox(width: 5),
-                    Text(
-                      authorName, 
-                      style: Theme.of(context).textTheme.caption, 
-                      strutStyle: StrutStyle(height: 1.25),
+        children: <Widget>[
+          InkWell(
+            child: Container(
+              padding: EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(title.trim(),
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w700)),
+                  SizedBox(height: 10),
+                  Container(
+                    constraints: BoxConstraints.expand(height: 20),
+                    child: Row(
+                      //crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(Icons.face, size: 15),
+                        SizedBox(width: 5),
+                        Text(
+                          authorName,
+                          style: Theme.of(context).textTheme.caption,
+                          strutStyle: StrutStyle(height: 1.25),
+                        ),
+                        SizedBox(width: 10),
+                        Icon(Icons.reply, size: 15),
+                        SizedBox(width: 5),
+                        Text(
+                          totalReplies.toString(),
+                          style: Theme.of(context).textTheme.caption,
+                          strutStyle: StrutStyle(height: 1.25),
+                        ),
+                        SizedBox(width: 10),
+                        Icon(Icons.access_time, size: 15),
+                        SizedBox(width: 5),
+                        Text(
+                          DateTimeFormat.relative(lastReply.toLocal(),
+                              abbr: true),
+                          style: Theme.of(context).textTheme.caption,
+                          strutStyle: StrutStyle(height: 1.25),
+                        ),
+                      ],
                     ),
-                    SizedBox(width: 10),
-                    Icon(Icons.reply, size: 15),
-                    SizedBox(width: 5),
-                    Text(
-                      totalReplies.toString(), 
-                      style: Theme.of(context).textTheme.caption, 
-                      strutStyle: StrutStyle(height: 1.25),
-                    ),
-                    SizedBox(width: 10),
-                    Icon(Icons.access_time, size: 15),
-                    SizedBox(width: 5),
-                    Text(
-                      DateTimeFormat.relative(lastReply.toLocal(), abbr: true), 
-                      style: Theme.of(context).textTheme.caption, 
-                      strutStyle: StrutStyle(height: 1.25),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
+            onTap: () => onTap(),
+            onLongPress: () => onLongPress(),
           ),
-        ),
-        onTap: () => onTap(),
-        onLongPress: () => onLongPress(),
-      ),
-      Divider(indent: 8,height: 1,thickness: 1),
-    ],
-  );
+          Divider(indent: 8, height: 1, thickness: 1),
+        ],
+      );
 }
