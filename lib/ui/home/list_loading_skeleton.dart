92594260730fc69dbb13028f381248c9c2f323e8
kazedayo <winsonlam@me.com>
2020-07-06 14:30:36 +0800

    minor UI tweak

--- lib/ui/home/list_loading_skeleton.dart
+++ lib/ui/home/list_loading_skeleton.dart
@@ -10,23 +10,32 @@ class ListLoadingSkeleton extends StatelessWidget {
                 physics: NeverScrollableScrollPhysics(),
                 children: List.generate(
                     50,
-                    (index) => ListTile(
-                          title: Container(
-                              margin: EdgeInsets.symmetric(vertical: 4),
-                              color: Colors.grey,
-                              height: 25),
-                          subtitle: Row(
-                            children: <Widget>[
-                              Flexible(
-                                flex: 2,
-                                child: Container(
-                                    margin: EdgeInsets.symmetric(vertical: 4),
-                                    color: Colors.grey[900],
-                                    height: 20),
-                              ),
-                              Spacer()
-                            ],
-                          ),
+                    (index) => Column(
+                          children: <Widget>[
+                            Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color: Colors.grey,
+                                ),
+                                margin: EdgeInsets.fromLTRB(10, 10, 10, 5),
+                                height: 25),
+                            Row(
+                              children: <Widget>[
+                                Flexible(
+                                  flex: 2,
+                                  child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(5),
+                                        color: Colors.grey,
+                                      ),
+                                      margin:
+                                          EdgeInsets.fromLTRB(10, 5, 10, 10),
+                                      height: 20),
+                                ),
+                                Spacer()
+                              ],
+                            ),
+                          ],
                         ))),
             baseColor: Theme.of(context).scaffoldBackgroundColor,
             highlightColor: Theme.of(context).splashColor),
