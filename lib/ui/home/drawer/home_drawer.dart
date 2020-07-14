f453ad06bbe306b7d784adbab33a7fb6fe7fdf48
kazedayo <winsonlam@me.com>
2020-07-14 22:52:21 +0800

    UI Tweaks

--- lib/ui/home/drawer/home_drawer.dart
+++ lib/ui/home/drawer/home_drawer.dart
@@ -18,9 +18,10 @@ class HomeDrawer extends StatelessWidget {
           children: <Widget>[
             HomeDrawerHeader(),
             GridView.builder(
+              physics: NeverScrollableScrollPhysics(),
               shrinkWrap: true,
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3, childAspectRatio: 2.7),
+                  crossAxisCount: 3, childAspectRatio: 2.6),
               itemCount: viewModel.channels.length,
               itemBuilder: (context, index) =>
                   ChannelCell(viewModel: viewModel, index: index),
