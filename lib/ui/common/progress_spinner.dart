7c6fc8f9aca9d355fb4e19ae8aef97c7a22a1fbc
kazedayo <winsonlam@me.com>
2020-08-22 16:26:38 +0800

    revert platform specific spinner

--- lib/ui/common/progress_spinner.dart
+++ lib/ui/common/progress_spinner.dart
@@ -4,25 +4,31 @@ import 'package:flutter/cupertino.dart';
 class ProgressSpinner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    switch (Theme.of(context).platform) {
-      case TargetPlatform.iOS:
-        return CupertinoActivityIndicator(radius: 9);
-        break;
-      case TargetPlatform.android:
-        return SizedBox.fromSize(
-          size: Size.square(15),
-          child: CircularProgressIndicator(
-              strokeWidth: 2,
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.grey)),
-        );
-        break;
-      default:
-        return SizedBox.fromSize(
-          size: Size.square(15),
-          child: CircularProgressIndicator(
-              strokeWidth: 2,
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.grey)),
-        );
-    }
+    // switch (Theme.of(context).platform) {
+    //   case TargetPlatform.iOS:
+    //     return CupertinoActivityIndicator(radius: 9);
+    //     break;
+    //   case TargetPlatform.android:
+    //     return SizedBox.fromSize(
+    //       size: Size.square(15),
+    //       child: CircularProgressIndicator(
+    //           strokeWidth: 2,
+    //           valueColor: AlwaysStoppedAnimation<Color>(Colors.grey)),
+    //     );
+    //     break;
+    //   default:
+    //     return SizedBox.fromSize(
+    //       size: Size.square(15),
+    //       child: CircularProgressIndicator(
+    //           strokeWidth: 2,
+    //           valueColor: AlwaysStoppedAnimation<Color>(Colors.grey)),
+    //     );
+    // }
+    return SizedBox.fromSize(
+      size: Size.square(15),
+      child: CircularProgressIndicator(
+          strokeWidth: 2,
+          valueColor: AlwaysStoppedAnimation<Color>(Colors.grey)),
+    );
   }
 }
