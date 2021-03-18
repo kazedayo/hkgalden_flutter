34a873435ba2051fa13851986a38c76679b8d1e1
kazedayo <winsonlam@me.com>
2021-03-18 15:19:25 +0800

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_redux/flutter_redux.dart';
